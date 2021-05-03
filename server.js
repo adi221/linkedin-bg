603a7371f122b61cebea2d35fd714c66e61c9ad9
adi221 <adimizrahi421@gmail.com>
2021-05-03 16:41:40 +0300

    sa

--- server.js
+++ server.js
@@ -52,6 +52,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 }
 
 app.listen(5000, () => console.log('Running on port 5000'));
